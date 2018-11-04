56ec037e401f3fd0a94d277db95f4426c5ec905e
nevernervous <nevernervous@email.com>
2018-11-05 03:47:12 +0800

    added profile, showering_data model and admin page.

--- wifi_shower/admin.py
+++ wifi_shower/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from .models import Device
+from .models import Device, Profile
 
 
 class DeviceAdmin(admin.ModelAdmin):
+    fields = ('mac_id', 'secret_key', 'created_date', 'sold_date', 'user')
     readonly_fields = ('created_date',)
     list_display = ('id', 'mac_id', 'secret_key', 'created_date', 'sold_date', 'user')
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('name', 'user', 'preheat_cycle', 'shower_cycle', 'shower_temp', 'old_shower_habits', 'water_used',
+              'water_saved', 'challenge_level', 'created_date', 'last_shower_date')
+    readonly_fields = ('preheat_cycle', 'shower_cycle', 'shower_temp', 'old_shower_habits', 'water_used',
+              'water_saved', 'challenge_level', 'created_date', 'last_shower_date')
+    list_display = ('id', 'name', 'user', 'preheat_cycle', 'shower_cycle', 'shower_temp', 'old_shower_habits', 'water_used',
+                    'water_saved', 'challenge_level', 'created_date', 'last_shower_date')
+
+
 admin.site.register(Device, DeviceAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- wifi_shower/models.py
+++ wifi_shower/models.py
@@ -14,4 +14,41 @@ class Device(models.Model):
         db_table = 'device'
 
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    preheat_cycle = models.FloatField(null=True, blank=True)
+    shower_cycle = models.FloatField(null=True, blank=True)
+    shower_temp = models.FloatField(null=True, blank=True)
+    old_shower_habits = models.FloatField(null=True, blank=True)
+    water_used = models.FloatField(null=True, blank=True)
+    water_saved = models.FloatField(null=True, blank=True)
+    challenge_level = models.FloatField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_shower_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'profile'
+
+
+class ShoweringData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    # 0: norma1: challenge
+    shower_mode = models.IntegerField(default=0)
+    preheat_cycle = models.FloatField(null=True, blank=True)
+    shower_cycle = models.FloatField(null=True, blank=True)
+    old_shower_habits = models.FloatField(null=True, blank=True)
+    water_used = models.FloatField(null=True, blank=True)
+    water_saved = models.FloatField(null=True, blank=True)
+    shower_temp = models.FloatField(null=True, blank=True)
+    mixing_temp = models.FloatField(null=True, blank=True)
+    challenge_level = models.FloatField(null=True, blank=True)
+    aggregate_water_used = models.FloatField(null=True, blank=True)
+    average_water_used = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'showering_data'
 
